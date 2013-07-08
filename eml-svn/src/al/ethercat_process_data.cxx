25c23f09db8ad98db9c7e4b8cb46dc69dac9703e
Austin Hendrix <ahendrix@willowgarage.com>
2013-07-08 16:09:16 -0700

    Rebase from 'debian/hydro/eml'

--- eml-svn/src/al/ethercat_process_data.cxx
+++ eml-svn/src/al/ethercat_process_data.cxx
@@ -51,16 +51,27 @@ EtherCAT_PD_Buffer::instance()
 // al_instance cannot be initiated right now, since this results in a
 // circular instantiation al_instance calls router_instance etc.
 EtherCAT_PD_Buffer::EtherCAT_PD_Buffer()
-  : m_is_running(0),
-    m_lrw_telegram(0x00,0x00010000,0x00,0,NULL), 
-    m_lrw_frame(&m_lrw_telegram)
+  : m_is_running(0)
 {
+  for (unsigned i=0;i<MAX_CHUNKS;++i) {
+    m_lrw_telegram[i] = new LRW_Telegram(0x00,0x00010000,0x00,0,NULL);
+    m_lrw_frame[i] = new EC_Ethernet_Frame(m_lrw_telegram[i]);
+  }
+
   // get pointer to DLL and logic
   m_dll_instance = EtherCAT_DataLinkLayer::instance();
   m_logic_instance = EC_Logic::instance();
 }
 
-EtherCAT_PD_Buffer::~EtherCAT_PD_Buffer(){}
+EtherCAT_PD_Buffer::~EtherCAT_PD_Buffer()
+{
+  for (unsigned i=0;i<MAX_CHUNKS;++i) {
+    delete m_lrw_telegram[i];
+    m_lrw_telegram[i] = NULL;
+    delete m_lrw_frame[i];
+    m_lrw_frame[i];
+  }
+}
 
 void 
 EtherCAT_PD_Buffer::start()
@@ -74,27 +85,70 @@ EtherCAT_PD_Buffer::stop()
   if (m_is_running > 0)
     --m_is_running;
   else
-    log(EC_LOG_INFO, "EtherCAT_PD_Buffer already stopped...\n");
+    ec_log(EC_LOG_INFO, "EtherCAT_PD_Buffer already stopped...\n");
 }
 
 bool
 EtherCAT_PD_Buffer::txandrx(size_t datalen, unsigned char * data)
 {
+  //define MAX_CHUNKS 4
+  //define CHUNK_SIZE 1486
+
+  if (datalen>(CHUNK_SIZE*MAX_CHUNKS)) {
+    ec_log(EC_LOG_ERROR, "PD_Buffer: Too much data (%zd) to send in %d chunks of %d bytes\n", 
+           datalen, MAX_CHUNKS, CHUNK_SIZE);
+    return false;
+  }
+
+  int handles[MAX_CHUNKS];
+  for (unsigned i=0;i<MAX_CHUNKS;++i)
+    handles[i]=-1;
+
+  bool success = true;
   if ( m_is_running != 0)
     // In case only starting when all slaves in the appropriate state
     // this becomes something like
     // if ( EtherCAT_PD_Buffer::m_al_instance->get_num_slaves() == m_running)
     {
-      m_lrw_telegram.set_idx(m_logic_instance->get_idx());
-      m_lrw_telegram.set_wkc(m_logic_instance->get_wkc());
-      m_lrw_telegram.set_datalen(datalen);
-      m_lrw_telegram.set_data(data);
-      if (!(m_dll_instance->txandrx(&m_lrw_frame))){
-	log(EC_LOG_ERROR, "PD_Buffer: Error sending PD\n");
-	return false;
+      int dst = 0x00010000;
+      unsigned index=0;
+      while (datalen > 0)
+      {
+        assert(index<MAX_CHUNKS);
+        size_t chunk_size = datalen <  CHUNK_SIZE ? datalen : CHUNK_SIZE;
+        LRW_Telegram* a_lrw_telegram(m_lrw_telegram[index]);
+        a_lrw_telegram->set_idx(m_logic_instance->get_idx());
+        a_lrw_telegram->set_wkc(m_logic_instance->get_wkc());
+        a_lrw_telegram->set_datalen(chunk_size);
+        a_lrw_telegram->set_data(data);
+        a_lrw_telegram->set_adr(dst);
+
+        int result = m_dll_instance->tx(m_lrw_frame[index]);
+        if (result < 0)
+        {
+          ec_log(EC_LOG_ERROR, "PD_Buffer: Error sending PD\n");
+          success = false;
+          break;
+        }
+        handles[index] = result;
+        datalen -= chunk_size;
+        data += chunk_size;
+        dst += chunk_size;
+        ++index;
+      }
+
+      // Receive every packet that was sent  
+      for(int i=index-1; i>=0; --i) {
+        if (handles[i] != -1) {
+          if (!m_dll_instance->rx(m_lrw_frame[i],handles[i])) {
+            ec_log(EC_LOG_ERROR, "PD_Buffer: Error receiving PD\n");
+            success = false;
+          }
+        }
       }
     }
-  return true;
+  
+  return success;
 }
 
 
